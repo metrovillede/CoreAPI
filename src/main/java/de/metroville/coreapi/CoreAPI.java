6a985b29017b653bf10b1a7ff1fd0d784cd2fb56
Luca Schwarz <business.whyauthentic@gmail.com>
2025-03-17 05:37:24 +0100

    feat(core-api): add support for managing both CorePlugin and JavaPlugin instances

--- src/main/java/de/metroville/coreapi/CoreAPI.java
+++ src/main/java/de/metroville/coreapi/CoreAPI.java
@@ -1,22 +1,34 @@
 package de.metroville.coreapi;
 
-public class CoreAPI {
-    private static CorePlugin instance;
+import org.bukkit.plugin.java.JavaPlugin;
 
-    public static void setInstance(CorePlugin core) {
-        instance = core;
+public class CoreAPI {
+    private static CorePlugin corePluginInstance;
+    private static JavaPlugin javaPluginInstance;
+
+    public static void setCorePluginInstance(CorePlugin corePlugin) {
+        corePluginInstance = corePlugin;
     }
 
-    public static CorePlugin getInstance() {
-        if (instance == null) {
+    public static void setJavaPluginInstance(JavaPlugin javaPlugin) {
+        javaPluginInstance = javaPlugin;
+    }
+
+    public static CorePlugin getCorePluginInstance() {
+        if (corePluginInstance == null) {
             throw new IllegalStateException("The CorePlugin is not registered!");
         }
-        return instance;
+        return corePluginInstance;
+    }
+
+    public static JavaPlugin getJavaPluginInstance() {
+        if (javaPluginInstance == null) {
+            throw new IllegalStateException("The JavaPlugin is not registered!");
+        }
+        return javaPluginInstance;
     }
 
     public static void registerModule(CoreModule module) {
-        getInstance().registerModule(module);
+        getCorePluginInstance().registerModule(module);
     }
-
-
 }
